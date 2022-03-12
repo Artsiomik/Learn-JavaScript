b19b7a57f4b90de67cff2fa21e8f374604da010b
Artsiom <Artyom.artyom.87@bk.ru>
2022-03-12 23:40:41 +0300

    add lesson

---
+++ урок 8. Циклы While, Do While/js/script.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 6; i++) {
+	console.log(i);
+}
+
+// найти сумму чисел от 0 до 10
+
+let sum = 0;
+let p = 0;
+while (p <= 10) {
+	sum = sum + p;
+	p++;
+}
+console.log('sum = ' + sum);
